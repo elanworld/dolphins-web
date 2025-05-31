58c2a1f3d2e8b4e792ad513806dab19fcc93315b
alan <elanworld@163.com>
2025-05-31 04:45:20 +0000

    api 更新

--- src/service/Api.vue
+++ src/service/Api.vue
@@ -6,8 +6,8 @@ class Api {
 
   mockPro = false
   preLink = process.env.NODE_ENV !== "development" || this.mockPro ? "https://www.xianneng.top/api/launcher-box" : "http://localhost:8030"
-  historyPage = this.preLink + "/chromeHistory/page"
-  scheduleTask = this.preLink + "/scheduleTask/list"
+  historyPage = this.preLink + "/chromeHistoryExt/page"
+  scheduleTask = this.preLink + "/scheduleTaskExt/list"
   schedule = this.preLink + "/com/manual/schedule"
 }
 
